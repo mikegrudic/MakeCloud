35446ea587f79fb1fbc8a0cbe044d45791e31eda
Mike Grudic <mgrudich@caltech.edu>
2017-07-03 13:53:28 -0700

    new meshless velocity correlation method

--- MakeCloud.py
+++ MakeCloud.py
@@ -17,7 +17,9 @@ Options:
    --bturb=<f>          Magnetic energy as a fraction of the Keplerian kinetic energy at a given radius [default: 0.01]
    --minmode=<N>        Minimum populated turbulent mode [default: 4]
    --turb_index=<N>     Power-law index of turbulent spectrum [default: 2]
+   --turb_meshless      Use new method for generating ~k^-2 velocity power spectrum without a grid (SLOW)
    --poisson            Use random particle positions instead of a gravitational glass
+   --scturb             Use self-consistent turbulent density and velocity from isothermal large-eddy simulation
    --seed=<N>           Random seed for generating particle positions, if --poisson is used [default: 42]
    --turb_path=<name>   Contains the root path of the turb [default: /home/mgrudic/scripts/MakeCloud/turb]
    --glass_path=<name>  Contains the root path of the glass ic [default: /home/mgrudic/glass_orig.npy]
@@ -28,6 +30,7 @@ from scipy import fftpack, interpolate, ndimage
 from scipy.integrate import quad, odeint, solve_bvp
 import sys
 import h5py
+from numba import jit
 from docopt import docopt
 
 arguments = docopt(__doc__)
@@ -40,6 +43,8 @@ S = float(arguments["--S"])
 turbulence = float(arguments["--turb"])
 magnetic_field = float(arguments["--bturb"])
 poisson = arguments["--poisson"]
+turb_meshless = arguments["--turb_meshless"]
+scturb = arguments["--scturb"]
 seed = int(float(arguments["--seed"])+0.5)
 turb_index = float(arguments["--turb_index"])
 minmode = int(arguments["--minmode"])
@@ -52,13 +57,42 @@ if filename==None:
     filename = "M%g_"%(1e10*M_gas) + ("MBH%g_"%(1e10*M_BH) if M_BH>0 else "") + "R%g_S%g_T%g_B%g_Res%d"%(R*1e3,S,turbulence,magnetic_field,res_effective) + ".hdf5"
     filename = filename.replace("+","").replace('e0','e')
 
-def TurbVelField(coords, res):
-    vt = np.load(turb_path+ "/vturb%d_n%d.npy"%(minmode,turb_index))
-    x = np.linspace(-R,R,vt.shape[0])
-    v = []
-    for i in xrange(3):
-        v.append(interpolate.interpn((x,x,x),vt[:,:,:,i], coords))
-    return np.array(v).T
+@jit(nopython=True)
+def CorrelateVelocities(x,v,soft=0.):
+    soft = soft*soft
+    N = len(v)
+    vnew = np.zeros_like(v)
+    xi = np.zeros(3)
+    distSqr = 0.
+    for i in xrange(N):
+        xi[0] = x[i,0]
+        xi[1] = x[i,1]
+        xi[2] = x[i,2]
+        if i%1000==0: print(i)
+        for j in xrange(N):
+            if i==j: continue
+            distSqr = 0.
+            for k in xrange(3):
+                dx = xi[k]-x[j,k]
+                distSqr += dx*dx
+            for k in xrange(3):
+                vnew[i,k] += v[j,k]/(distSqr+soft)**0.5
+    return vnew
+    
+def TurbVelField(coords, res, meshless=False):
+    if not meshless:
+        vt = np.load(turb_path + "/vturb%d_n%d.npy"%(minmode,turb_index))
+        x = np.linspace(-R,R,vt.shape[0])
+        v = []
+        for i in xrange(3):
+            v.append(interpolate.interpn((x,x,x),vt[:,:,:,i], coords))
+        return np.array(v).T
+    else:
+        v = np.random.normal(size=coords.shape)
+        print R/res
+        v = CorrelateVelocities(coords, v, R/res)
+        return v
+
 
 def TurbBField(coords, res):
     vt = np.load(turb_path + "/bturb%d_n%d.npy"%(minmode, turb_index))
@@ -74,28 +108,49 @@ G = 4.3e4
 if poisson:
     np.random.seed(seed)
     x = 2*(np.random.rand(2*N_gas, 3)-0.5)
+elif scturb:
+    x = 2 * (np.load(turb_path+"/xturb.npy") - 0.5)
+    vturb = np.load(turb_path + "/vturb.npy")
 else:
     x = 2*(np.load(glass_path)-0.5)
+    if len(x) < N_gas:
+        x = 2*(np.load("/home/mgrudic/glass.npy")-0.5)
+#        from itertools import product
+#        print x.max(), x.min()
+#        x = np.concatenate([x/2 + 0.5 + np.array([i,j,k])/2 for i,j,k in product(range(2),range(2),range(2))]) - 0.5
+#        x *= 2
+#        print np.max(x,axis=0)#, x.min()
 Nx = len(x)
 r = np.sum(x**2, axis=1)**0.5
-x = x[r.argsort()][:N_gas]
 
-x *= (float(Nx) / N_gas * 4*np.pi/3 / 8)**(1./3)
+if scturb:
+    x, vturb = x[r<= 1.], vturb[r<=1.]
+
+    xchoice = np.random.choice(np.arange(len(x)),size=N_gas,replace=False)
+    x, vturb = x[xchoice], vturb[xchoice]
+else:
+    x = x[r.argsort()][:N_gas]
+    x *= (float(Nx) / N_gas * 4*np.pi/3 / 8)**(1./3)
 x = x - np.average(x,axis=0)
 r = np.sum(x**2, axis=1)**0.5
-x, r = x[r.argsort()], r[r.argsort()]
 
-x, r = x/r.max(), r/r.max()
 
-rho_form = lambda r: 1. #change this function to get a different radial density profile; normalization does not matter as long as rmin and rmax are properly specified
-rho_norm = quad(lambda r: rho_form(r) * 4 * np.pi * r**2, rmin, R)[0]
-rho = lambda r: rho_form(r) / rho_norm
 
-rnew = odeint(lambda rphys, r3: np.exp(r3)/(4*np.pi*np.exp(rphys)**3*rho(np.exp(rphys))), np.log(R), np.log(r[::-1]**3), atol=1e-12, rtol=1e-12)[::-1,0]
-rnew = np.exp(rnew)
+if not scturb:
+    x, r = x[r.argsort()], r[r.argsort()]
+    x, r = x/r.max(), r/r.max()
+    rho_form = lambda r: 1. #change this function to get a different radial density profile; normalization does not matter as long as rmin and rmax are properly specified
+    rho_norm = quad(lambda r: rho_form(r) * 4 * np.pi * r**2, rmin, R)[0]
+    rho = lambda r: rho_form(r) / rho_norm
 
-x=(x.T * rnew/r).T
-r = np.sum(x**2, axis=1)**0.5
+    rnew = odeint(lambda rphys, r3: np.exp(r3)/(4*np.pi*np.exp(rphys)**3*rho(np.exp(rphys))), np.log(R), np.log(r[::-1]**3), atol=1e-12, rtol=1e-12)[::-1,0]
+    rnew = np.exp(rnew)
+
+    x=(x.T * rnew/r).T
+    r = np.sum(x**2, axis=1)**0.5
+else:
+    x, r = x*R/r.max(), r*R/r.max()
+    print x.max(), x.min()
 #just some useful unit vectors...
 #r2 = np.sum(x[:,:2]**2,axis=1)**0.5
 #r = np.sum(x**2, axis=1)**0.5
@@ -107,7 +162,10 @@ mgas = np.repeat(M_gas/N_gas, N_gas)
 
 Mr = M_BH + mgas.cumsum()
 
-omega = (G*Mr/r**3)**0.5
+if not scturb:
+    omega = (G*Mr/r**3)**0.5
+else:
+    omega = (G * M_gas / R**3)**0.5
 
 v_K = omega * r
 
@@ -117,7 +175,9 @@ v = S*np.cross(np.c_[np.zeros_like(omega),np.zeros_like(omega),omega], x)
 ugrav = G * np.sum(Mr/ r * mgas)
 
 if turbulence>0.0:
-    vturb = TurbVelField(x, res=res_effective)
+    if not scturb:
+        vturb = TurbVelField(x, res=res_effective,meshless=turb_meshless)
+        print vturb.shape
     vturb = (vturb.T * omega).T
     mvturbSqr = 0.5*M_gas/N_gas*np.sum(vturb*vturb)
     v += vturb*np.sqrt(turbulence*ugrav/mvturbSqr)
