0f919bf11a37c5908c6e355436256dab6df5dcc0
guszejnov <guszejnov.david@gmail.com>
2019-03-27 13:31:34 -0500

    Added ability to make very large IC files

--- MakeCloud.py
+++ MakeCloud.py
@@ -125,7 +125,7 @@ if GMC_units:
 
 if localdir:
     turb_path = "turb"
-    glass_path = "glass.npy"
+    glass_path = "glass_256.npy"
 
 if arguments["--boxsize"] is not None:
 #    print(arguments["--boxsize"])
@@ -215,11 +215,23 @@ else:
     x = 2*(np.load(glass_path)-0.5)
     Nx = len(x)
     if len(x)*np.pi*4/3 / 8 < N_gas:
+        print("Default glass too small (N=%d while N_gas=%d), trying alternative"%(Nx,N_gas))
         if localdir:
-            x = 2*(np.load("glass_256.npy")-0.5)
+            x = 2*(np.load("glass.npy")-0.5)
         else:
             x = 2*(np.load("/home/mgrudic/glass.npy")-0.5)
-        
+    #Check again
+    Nx = len(x); Nratio=Nx*np.pi*4/3 / 8/N_gas
+    if Nratio < 1:
+        #Constructing new glass
+        n1d=int(np.floor((N_gas*8/(np.pi*4/3))**(0.3333))+2)
+        if (n1d % 2 != 0):
+            n1d+=1 #to avoid odd numbers which would put a particle at the center
+        print("Saved glasses are too small (N=%d while N_gas=%d), constructing new glass with %d^3 particles..."%(Nx,N_gas,n1d))
+        xv = np.linspace(-1.0, 1.0, n1d);Nx=n1d**3
+        x=np.zeros([Nx,3])
+        x1, x2, x3 = np.meshgrid(xv,xv,xv)
+        x[:,0]=x1.flatten(); x1=0; x[:,1]=x2.flatten(); x2=0; x[:,2]=x3.flatten(); x3=0;
     r = np.sum(x**2, axis=1)**0.5
     x = x[r.argsort()][:N_gas]
     x *= (float(Nx) / N_gas * 4*np.pi/3 / 8)**(1./3)*R
